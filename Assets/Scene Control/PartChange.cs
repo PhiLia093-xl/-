9293e1776d4fbeb2d93358764852534257658118
Amazia9 <3380927114@qq.com>
2025-12-23 21:29:20 +0800

    场景创建、切换代码与图片

---
+++ Assets/Scene Control/PartChange.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PartChange : MonoBehaviour
+{
+
+    public void Part1()
+    {
+        SceneManager.LoadScene("Part1");
+    }
+
+    public void Part2()
+    {
+        SceneManager.LoadScene("Part2");
+    }
+    public void Part3()
+    {
+        SceneManager.LoadScene("Part3");
+    }
+    public void Part4()
+    {
+        SceneManager.LoadScene("Part4");
+    }
+    public void Part5()
+    {
+        SceneManager.LoadScene("Part5");
+    }
+    public void Part6()
+    {
+        SceneManager.LoadScene("Part6");
+    }
+    public void Part7()
+    {
+        SceneManager.LoadScene("Part7");
+    }
+    public void Part8()
+    {
+        SceneManager.LoadScene("Part8");
+    }
+    public void Part9()
+    {
+        SceneManager.LoadScene("Part9");
+    }
+    public void Part10()
+    {
+        SceneManager.LoadScene("Prat10");
+    }
+    public void Part11()
+    {
+        SceneManager.LoadScene("Part11");
+    }
+    public void Part12()
+    {
+        SceneManager.LoadScene("Part12");
+    }
+    public void Part13()
+    {
+        SceneManager.LoadScene("Part13");
+    }
+}
